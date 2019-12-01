23f2a8223dba6f0812833bf42a77f423748db2f3
edzempel <edward.zempel@gmail.com>
2019-12-01 15:58:15 -0600

    AdjacencyMatrix : not sure if usable

--- src/edu/metrostate/by8477ks/ics340/p3/AdjacencyMatrixGraph.java
+++ src/edu/metrostate/by8477ks/ics340/p3/AdjacencyMatrixGraph.java
@@ -1,4 +1,11 @@
 package edu.metrostate.by8477ks.ics340.p3;
 
-public class Model {
+import java.util.ArrayList;
+
+public class AdjacencyMatrixGraph {
+    String[] graph;
+
+     public AdjacencyMatrixGraph(ArrayList<String> locations){
+         this.graph = new String[locations.size()];
+     }
 }
